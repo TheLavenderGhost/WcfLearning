53e667ea6e7a279077c124f3b6cdde3bb6c86286
Katarzyna Sajchta <62062768+TheLavenderGhost@users.noreply.github.com>
2022-08-28 18:32:18 +0200

    clearing redundant auto-generated data

---
+++ WcfServiceLibrary/HelloService.cs
@@ -0,0 +1,10 @@
+﻿namespace WcfServiceLibrary
+{
+    public class HelloService : IHelloService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
